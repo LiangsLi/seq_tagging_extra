a46a22eba092c87dbc684d5509655fa12402829d
LiangsLi <poales@sina.com>
2018-07-23 12:27:02 +0800

    gpu

--- train.py
+++ train.py
@@ -6,20 +6,21 @@ from model.config import Config
 def main():
     # create instance of config
     config = Config()
-
+    
     # build model
     model = NERModel(config)
     model.build()
     # model.restore_session(config.dir_model) # optional, restore weights
     # model.reinitialize_weights("proj")
-
+    
     # create datasets
-    dev   = CoNLLDataset(config.filename_dev, config.processing_word,
-                         config.processing_tag, config.max_iter)
+    dev = CoNLLDataset(config.filename_dev, config.processing_word,
+                       config.processing_tag, config.max_iter, max_iter=1000)
     train = CoNLLDataset(config.filename_train, config.processing_word,
-                         config.processing_tag, config.max_iter)
+                         config.processing_tag, config.max_iter, max_iter=1000)
     # train model
     model.train(train, dev)
+
 
 if __name__ == "__main__":
     main()
